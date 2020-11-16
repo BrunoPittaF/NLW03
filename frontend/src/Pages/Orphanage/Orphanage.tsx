07da1dabcd569d9caf854e566fa6045bd9ae3c98
Caio Bruno <caiobrunopittaf@gmail.com>
2020-11-16 16:08:25 -0300

    finalizado detalhe do orfanato

--- frontend/src/Pages/Orphanage/Orphanage.tsx
+++ frontend/src/Pages/Orphanage/Orphanage.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { FaWhatsapp } from "react-icons/fa";
+//import { FaWhatsapp } from "react-icons/fa";
 import { FiClock, FiInfo } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-import './orphanage.css';
+import "./orphanage.css";
 import Sidebar from "../../components/Sidebar";
 import mapIcon from "../../utils/mapIcon";
 import api from "../../services/api";
-import OrphanagesMap from "../OrphanagesMap";
+//import OrphanagesMap from "../OrphanagesMap";
 
 interface Orphanage {
   latitude: number;
@@ -24,7 +24,7 @@ interface Orphanage {
   }>;
 }
 
-interface OrphanageParams{
+interface OrphanageParams {
   id: string;
 }
 
@@ -32,18 +32,16 @@ const Orphanage: React.FC = () => {
   const params = useParams<OrphanageParams>();
 
   const [orphanage, setOrphanage] = useState<Orphanage>();
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
 
-
-  
   useEffect(() => {
-    api.get(`orphanages/${params.id}`).then(response =>{
+    api.get(`orphanages/${params.id}`).then((response) => {
       setOrphanage(response.data);
-
-    }) 
+    });
   }, [params.id]);
 
   if (!orphanage) {
-    return <p>Carregando...</p>
+    return <p>Carregando...</p>;
   }
 
   return (
@@ -52,50 +50,67 @@ const Orphanage: React.FC = () => {
 
       <main>
         <div className="orphanage-details">
-          <img src={orphanage.images[0].url} alt={orphanage.name} />
+          <img src={orphanage.images[activeImageIndex].url} alt={orphanage.name} />
 
           <div className="images">
-            {orphanage.images.map(image => {
+            {orphanage.images.map((image,index) => {
               return (
-              <button key={image.id} className="active" type="button">
-              <img src={image.url} alt={orphanage.name} />
-            </button>
-              )
+                <button 
+                key={image.id} 
+                className={activeImageIndex == index ? 'active' : ''} 
+                type="button"
+                onClick={() => {
+                  setActiveImageIndex(index)
+                }}
+                >
+                  <img src={image.url} alt={orphanage.name} />
+                </button>
+              );
             })}
           </div>
-          
+
           <div className="orphanage-details-content">
             <h1>{orphanage.name}</h1>
-            <p>
-              {orphanage.about}
-            </p>
+            <p>{orphanage.about}</p>
 
             <div className="map-container">
-              <Map 
-                center={[orphanage.latitude,  orphanage.longitude]} 
-                zoom={16}  
-                style={{ width: '100%', height: 280 }}
+              <Map
+                center={[orphanage.latitude, orphanage.longitude]}
+                zoom={16}
+                style={{ width: "100%", height: 280 }}
                 dragging={false}
                 touchZoom={false}
                 zoomControl={false}
                 scrollWheelZoom={false}
                 doubleClickZoom={false}
               >
-                <TileLayer 
+                <TileLayer
                   url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
-                <Marker interactive={false} icon={ mapIcon } position={[orphanage.latitude,  orphanage.longitude]} />
+                <Marker
+                  interactive={false}
+                  icon={mapIcon}
+                  position={[orphanage.latitude, orphanage.longitude]}
+                />
               </Map>
 
               <footer>
-                <a href="">Ver rotas no Google Maps</a>
+                <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}
+                >
+                  Ver rotas no Google Maps
+                </a>
               </footer>
             </div>
 
             <hr />
 
             <h2>{orphanage.instructions}</h2>
-            <p>Venha como se sentir mais à vontade e traga muito amor para dar.</p>
+            <p>
+              Venha como se sentir mais à vontade e traga muito amor para dar.
+            </p>
 
             <div className="open-details">
               <div className="hour">
@@ -105,16 +120,17 @@ const Orphanage: React.FC = () => {
               </div>
               {orphanage.open_on_weekends ? (
                 <div className="open-on-weekends">
-                <FiInfo size={32} color="#39CC83" />
-                Atendemos <br />
-                fim de semana
-              </div>
-              ) : 
-              <div className="open-on-weekends dont-open">
-              <FiInfo size={32} color="#FF669D" />
-              Não atendemos <br />
-              fim de semana
-            </div>}
+                  <FiInfo size={32} color="#39CC83" />
+                  Atendemos <br />
+                  fim de semana
+                </div>
+              ) : (
+                <div className="open-on-weekends dont-open">
+                  <FiInfo size={32} color="#FF669D" />
+                  Não atendemos <br />
+                  fim de semana
+                </div>
+              )}
             </div>
 
             {/*<button type="button" className="contact-button">
@@ -126,6 +142,6 @@ const Orphanage: React.FC = () => {
       </main>
     </div>
   );
-}
+};
 
 export default Orphanage;
