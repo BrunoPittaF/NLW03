c03a7706e277d7ebfe2020aeac3ad4766293bedf
Caio Bruno <caiobrunopittaf@gmail.com>
2020-11-16 16:46:06 -0300

    formulario de criaçao

--- frontend/src/Pages/Orphanage/Orphanage.tsx
+++ frontend/src/Pages/Orphanage/Orphanage.tsx
@@ -57,7 +57,7 @@ const Orphanage: React.FC = () => {
               return (
                 <button 
                 key={image.id} 
-                className={activeImageIndex == index ? 'active' : ''} 
+                className={activeImageIndex === index ? 'active' : ''} 
                 type="button"
                 onClick={() => {
                   setActiveImageIndex(index)
